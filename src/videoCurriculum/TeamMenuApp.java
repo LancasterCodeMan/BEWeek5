45677534c2004cb60d70cf7e9dceb76415ca057d
LancasterCodeMan <126622005+LancasterCodeMan@users.noreply.github.com>
2023-08-03 16:43:49 -0500

    Working on Menu app

--- src/videoCurriculum/TeamMenuApp.java
+++ src/videoCurriculum/TeamMenuApp.java
@@ -3,7 +3,17 @@ package videoCurriculum;
 public class TeamMenuApp {
 
 	public static void main(String[] args) {
+		
+	Player p1 = new Player("Thomas", "Fullback", "Running Fast");
+	Player p2 = new Player("Sally", "Quarterback", "Throwing Far");
+	
+	Team team = new Team("Really Good Football Team!!!");
+	team.addPlayer(p1);
+	team.addPlayer(p2);
+	team.describe();
 
+	team.removePlayer("Thomas");
+	team.describe();
 	}
 
 }
