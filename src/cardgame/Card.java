103906219338be1494c2b0739d12b93295576edb
LancasterCodeMan <126622005+LancasterCodeMan@users.noreply.github.com>
2023-08-09 20:39:58 -0500

    worked on cardgame

--- src/cardgame/Card.java
+++ src/cardgame/Card.java
@@ -1,20 +1,45 @@
+//
+// Copyright (c) 2023 Promineo Tech
+// Author:  Promineo Tech Academic Team
+// Subject:  Object Oriented Programming
+// Java Week 05 Lab Solution 
+//
 package cardgame;
 
 public class Card {
-	
-	private String name;
-	private String suit;
-	private int value;
-	
-	public Card (String name, String suit, int value) {
+	 String name;
+	 String suit;
+	 int value;
+	 
+	 public Card(String name, String suit, int value){
+		 this.name = name;
+		 this.suit = suit;
+		 this.value = value;
+		 
+	 }
+	 
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
 		this.name = name;
+	}
+	public String getSuit() {
+		return suit;
+	}
+	public void setSuit(String suit) {
 		this.suit = suit;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
 		this.value = value;
 	}
-	
+
 	public void describe() {
-		System.out.println(name + " of " + suit + ": " + value);
-	
+		System.out.println(this.name + " of " + this.suit + ": " + this.value);
+		
 	}
 
 }
